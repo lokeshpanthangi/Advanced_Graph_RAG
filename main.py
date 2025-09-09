9404212eeaec360a71223d95033f87fa81d04b50
lokeshpanthangi <praharshithapanthangi@gmail.com>
2025-09-09 14:58:10 +0530

    Completed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import OpenAIEmbeddings,ChatOpenAI
 from langchain_pinecone import PineconeVectorStore
